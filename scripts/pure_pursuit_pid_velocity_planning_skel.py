4b120a066e76d1670bb8bbcaeb3d7b60ee474cd8
woong137 <woong137137@gmail.com>
2024-03-16 00:26:49 +0900

    코드 변경 내용: pure_pursuit_pid_velocity_planning_skel.py 파일에서 일부 함수 및 변수명 수정, 속도 비례 Look Ahead Distance 값 설정 변경, 도로 곡률 계산 방식 변경

--- scripts/pure_pursuit_pid_velocity_planning_skel.py
+++ scripts/pure_pursuit_pid_velocity_planning_skel.py
@@ -124,7 +124,7 @@ class pure_pursuit:
         self.current_postion.x = msg.pose.pose.position.x
         self.current_postion.y = msg.pose.pose.position.y
 
-    def status_callback(self, msg):  ## Vehicl Status Subscriber
+    def status_callback(self, msg):  ## Vehicle Status Subscriber
         self.is_status = True
         self.status_msg = msg
 
@@ -151,12 +151,8 @@ class pure_pursuit:
 
         # TODO: (2) 속도 비례 Look Ahead Distance 값 설정
         self.lfd = (self.status_msg.velocity.x) * self.lfd_gain
-
-        if self.lfd < self.min_lfd:
-            self.lfd = self.min_lfd
-        elif self.lfd > self.max_lfd:
-            self.lfd = self.max_lfd
-        rospy.loginfo(self.lfd)
+        self.lfd = np.clip(self.lfd, self.min_lfd, self.max_lfd)
+        rospy.loginfo("lfd: %f", self.lfd)
 
         vehicle_position = self.current_postion
         self.is_look_forward_point = False
@@ -222,27 +218,26 @@ class velocityPlanning:
         self.car_max_speed = car_max_speed
         self.road_friction = road_friciton
 
-    def curvedBaseVelocity(self, gloabl_path, point_num):
+    def curvedBaseVelocity(self, global_path, point_num):
         out_vel_plan = []
 
         for i in range(0, point_num):
             out_vel_plan.append(self.car_max_speed)
 
-        for i in range(point_num, len(gloabl_path.poses) - point_num):
+        for i in range(point_num, len(global_path.poses) - point_num):
             x_list = []
             y_list = []
             for box in range(-point_num, point_num):
-                x = gloabl_path.poses[i + box].pose.position.x
-                y = gloabl_path.poses[i + box].pose.position.y
+                x = global_path.poses[i + box].pose.position.x
+                y = global_path.poses[i + box].pose.position.y
                 x_list.append([-2 * x, -2 * y, 1])
                 y_list.append((-x * x) - (y * y))
 
-            # TODO: (6) 도로의 곡률 계산
+            # TODO: (6) 도로의 곡률 계산, PPT p.390
             x_matrix = np.array(x_list)
             y_matrix = np.array(y_list)
-            x_trans = x_matrix.T
+            a_matrix = np.linalg.pinv(x_matrix).dot(y_matrix)
 
-            a_matrix = np.linalg.inv(x_trans.dot(x_matrix)).dot(x_trans).dot(y_matrix)
             a = a_matrix[0]
             b = a_matrix[1]
             c = a_matrix[2]
@@ -255,10 +250,10 @@ class velocityPlanning:
                 v_max = self.car_max_speed
             out_vel_plan.append(v_max)
 
-        for i in range(len(gloabl_path.poses) - point_num, len(gloabl_path.poses) - 10):
+        for i in range(len(global_path.poses) - point_num, len(global_path.poses) - 10):
             out_vel_plan.append(30)
 
-        for i in range(len(gloabl_path.poses) - 10, len(gloabl_path.poses)):
+        for i in range(len(global_path.poses) - 10, len(global_path.poses)):
             out_vel_plan.append(0)
 
         return out_vel_plan
